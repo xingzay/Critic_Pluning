8ef56aabb71b346c9b7c6c16719ee561d76e8c30
xingzay <927348581@qq.com>
2024-06-11 17:03:55 +0800

    update ui

--- robots_qt/src/robots_qt/include/widget.h
+++ robots_qt/src/robots_qt/include/widget.h
@@ -1,18 +1,14 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
-#include <QLabel>
-#include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-#include <mutex>
 #include <QTimer>
 #include <QDateTime>
 #include <QProcess>
-#include <std_msgs/msg/empty.hpp>
-#include <sensor_msgs/msg/laser_scan.hpp>
-using std::placeholders::_1;
+#include <QWidget>
+#include <QLabel>
+#include <mutex>
+#include <thread>
+
 
 namespace Ui {
 class Widget;
@@ -31,45 +27,23 @@ class Widget : public QWidget
 
 public:
     explicit Widget(QWidget *parent = nullptr);
-    rclcpp::Node::SharedPtr node;
+    // rclcpp::Node::SharedPtr node;
     ~Widget();
-
-private:
+protected:
     Ui::Widget *ui;
-
+private:
+    // Ui::Widget *ui;
     std::thread spin_thread; //多线程对象
     std::mutex mutex_; //互斥锁
-
-    // 速度数据
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr vel_sub_;
-    void vel_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_vel);
-
-    // 里程计数据
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-    double total_distance_;  // 累计距离
-    nav_msgs::msg::Odometry last_odometry_;  // 上一次的里程计数据
-    bool first_msg_;  // 标记是否是第一次接收
-    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr odom);
-
-    // 判断turtlebot3_node节点是否启动
-    rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr heartbeat_sub_;
-    void heartbeat_callback(const std_msgs::msg::Empty&);
     QTimer* timer_;
-    rclcpp::Time last_heartbeat_;
-    void check_heartbeat();
 
-    // 判断激光雷达是否启动
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
-    void check_scan_topic(const sensor_msgs::msg::LaserScan & scan);
-    Point findMax(const sensor_msgs::msg::LaserScan& scan);
-    void updateGUI();
 
 signals:
     void dataReady(int index, float range, float intensity, float x, float y);
 
 
 private slots:
-
+    void updateGUI();
     void on_MainWindow_clicked();
 
     void on_Clear_clicked();
--- robots_qt/src/robots_qt/src/widget.cpp
+++ robots_qt/src/robots_qt/src/widget.cpp
@@ -5,11 +5,10 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget),
-    mutex_(),
-    last_heartbeat_(rclcpp::Clock().now())
+    mutex_()
 {
     ui->setupUi(this);
-
+    // connect(ui->pushButton_i,SIGNAL(clicked()),this,SLOT(slot_cmd_control()));
     // 将check_tb3_node_label以及check_robot_label默认背景颜色为红色
 
     ui->check_tb3_node_label->setStyleSheet("background-color: red;");
@@ -19,47 +18,15 @@ Widget::Widget(QWidget *parent)
 
     // 改变表格大小
     connect(this, &Widget::dataReady, this, &Widget::updateLaserTable);
-
-
-    int argc = 0; char **argv = NULL;
-    rclcpp::init(argc, argv);
-    node = rclcpp::Node::make_shared("ROS2_Node_QT");
-
-    vel_sub_ = node->create_subscription<geometry_msgs::msg::Twist>(
-        "/cmd_vel",
-        10,
-        std::bind(&Widget::vel_callback,this,_1));
-    // 路程 -- 里程计信息
-    odom_sub_ = node->create_subscription<nav_msgs::msg::Odometry>(
-        // "/odometry/filtered",
-        "/odom",
-        10,
-        std::bind(&Widget::odom_callback,this,_1)
-        );
-
-    // 机器人状态 -- 判断turtlebot3_node节点是否启动
-    heartbeat_sub_ = node->create_subscription<std_msgs::msg::Empty>(
-        "/heart_beat",
-        10,
-        std::bind(&Widget::heartbeat_callback,this,_1)
-        );
-    // 激光雷达 -- 判断是否订阅到/scan话题
-    laser_scan_sub_ = node->create_subscription<sensor_msgs::msg::LaserScan>(
-        "/scan",
-        10,
-        std::bind(&Widget::check_scan_topic,this,_1)
-    );
-
     // 创建定时器，用于检查心跳信号是否超时
     timer_ = new QTimer(this);
-    connect(timer_, &QTimer::timeout, this, &Widget::check_heartbeat);
     connect(timer_, &QTimer::timeout, this, &Widget::update_time);
     timer_->start(1000);
 
     //多线程运行spin
-    spin_thread = std::thread([this]() {
-        rclcpp::spin(node);
-    });
+    // spin_thread = std::thread([this]() {
+    //     rclcpp::spin(node);
+    // });
 
 }
 
@@ -73,127 +40,14 @@ Widget::~Widget()
     if (timer_) {
         timer_->stop();  // 停止定时器
     }
-    rclcpp::shutdown();
+    // rclcpp::shutdown();
     delete ui;
 }
+
 void Widget::updateGUI(){
     return;
 }
-// 显示速度
-void Widget::vel_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_vel){
-    std::lock_guard<std::mutex> lock(mutex_); // 确保互斥
-    // 构建显示文本
-    QString linear_text = QString::number(cmd_vel->linear.x,'f',3) + " m/s";
-    QString angular_text = QString::number(cmd_vel->angular.z,'f',3) + " rad/s";
-    // 确保在 Qt 主线程中更新 QLabel
-    ui->linear_label->setText(linear_text);
-    QMetaObject::invokeMethod(ui->linear_label, "setText", Qt::AutoConnection, Q_ARG(QString, linear_text));
-    ui->angular_label->setText(angular_text);
-    QMetaObject::invokeMethod(ui->angular_label, "setText", Qt::AutoConnection, Q_ARG(QString, angular_text));
-}
 
-// 显示总路程以及当前位置
-void Widget::odom_callback(const nav_msgs::msg::Odometry::SharedPtr odom){
-    std::lock_guard<std::mutex> lock(mutex_); // 确保互斥
-    if(first_msg_){
-        last_odometry_ = *odom;  // 如果是第一次接收，只存储数据
-        first_msg_ = false;
-        return;
-    }else{
-        // 计算相邻两次里程计之间的距离
-        double dx = odom->pose.pose.position.x - last_odometry_.pose.pose.position.x;
-        double dy = odom->pose.pose.position.y - last_odometry_.pose.pose.position.y;
-        double distance = std::sqrt(dx * dx + dy * dy);
-
-        total_distance_ += distance;  // 累加总距离
-        last_odometry_ = *odom;  // 更新上一次的里程计数据
-    }
-    // 利用里程计数据，计算小车走过的总路程
-    QString route_text = QString::number(total_distance_,'f',3) + "  m";
-    ui->route_label->setText(route_text);
-    QMetaObject::invokeMethod(ui->route_label, "setText", Qt::AutoConnection, Q_ARG(QString, route_text));
-
-    QString position_text =
-        QString("(%1, %2)").arg(QString::number(odom->pose.pose.position.x, 'f', 2)).
-                            arg(QString::number(odom->pose.pose.position.y, 'f', 2));
-    ui->robot_position_label->setText(position_text);
-
-}
-
-// 检测turtlebot3_node
-void Widget::heartbeat_callback(const std_msgs::msg::Empty& ) {
-    std::lock_guard<std::mutex> lock(mutex_); // 确保互斥
-    last_heartbeat_ = rclcpp::Clock().now();  // 更新心跳时间
-    // 收到心跳信号后，将颜色更改为绿色
-    QMetaObject::invokeMethod(
-        this,
-        [this]() {
-            ui->check_tb3_node_label->setStyleSheet("background-color: green;");
-            ui->check_robot_label->setStyleSheet("background-color: green;");
-        },
-        Qt::AutoConnection
-        );
-}
-
-void Widget::check_heartbeat() {
-    std::lock_guard<std::mutex> lock(mutex_);  // 确保线程安全
-    auto time_diff = (rclcpp::Clock().now() - last_heartbeat_).seconds();
-
-    if (time_diff > 3) {  // 如果超过3秒没有收到心跳信号
-        // 将颜色设为红色
-        QMetaObject::invokeMethod(
-            this,
-            [this]() {
-                ui->check_tb3_node_label->setStyleSheet("background-color: red;");
-                ui->check_robot_label->setStyleSheet("background-color: red;");
-                ui->check_scan_node_label->setStyleSheet("background-color: red;");
-            },
-            Qt::AutoConnection
-            );
-    }
-}
-
-void Widget::check_scan_topic(const sensor_msgs::msg::LaserScan & scan){
-    std::lock_guard<std::mutex> lock(mutex_);  // 确保线程安全
-    // 显示绿色 发布激光强度（频率要慢）
-    ui->check_scan_node_label->setStyleSheet("background-color: green;");
-    for (int i = 0; i < 15; i++){
-        auto max = findMax(scan);
-        emit dataReady(i, max.range, max.intensity, max.x, max.y);  // 发射信号
-        // std::cout << "激光最大数据:"
-        //           << " 距离:" << max.range << " 强度:" << max.intensity
-        //           << "坐标: (" << max.x << ", " << max.y << ")" << std::endl;
-    }
-    return ;
-}
-
-void Widget::updateLaserTable(int index, float range, float intensity, float x, float y) {
-    // setItem(行row，列column，内容)
-    ui->laser_tableWidget->setItem(index, 0, new QTableWidgetItem(QString::number(index)));
-    ui->laser_tableWidget->setItem(index, 1, new QTableWidgetItem(QString::number(range)));
-    ui->laser_tableWidget->setItem(index, 2, new QTableWidgetItem(QString::number(intensity)));
-    ui->laser_tableWidget->setItem(index, 3, new QTableWidgetItem(QString("(%1, %2)").arg(x).arg(y)));
-}
-
-Point Widget::findMax(const sensor_msgs::msg::LaserScan& scan)
-{
-    Point max{0, 0.0, 0.0, 0.0, 0.0};
-    for (std::vector<float>::size_type i = 0; i < scan.ranges.size(); i++)
-    {
-        double intensity = scan.intensities[i];
-        if (intensity > max.intensity)
-        {
-            max.index = i;
-            max.range = scan.ranges[i];
-            max.intensity = intensity;
-            // 角度转弧度
-            double angle = scan.angle_min + scan.angle_increment * max.index;
-            max.x = max.range * std::cos(angle);
-            max.y = max.range * std::sin(angle);
-        }
-    }
-    return max;
-}
 
 
 //主界面
@@ -317,5 +171,6 @@ void Widget::update_time(){
     QString timeString = currentTime.toString("yyyy-MM-dd hh:mm:ss");
     ui->update_time->setText(timeString);
 }
+
 
 
